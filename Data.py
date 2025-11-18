67ba0bf858026233e7a31ede36bf9d0844c6885f
Melissa Halley <melissamariahalley@outlook.com>
2025-11-18 22:22:59 +0100

    button click def

--- Data.py
+++ Data.py
@@ -1,1 +1,14 @@
-def persoonsgegevens(self, )
+import ttkbootstrap
+#Persoonsgegevens data
+class Persoonsgegevens:
+        def __init__(self, voornaam, achternaam, adres, postcode):
+            self.voornaam = voornaam
+            self.achternaam = achternaam
+            self.adres = adres
+            self.postcode = postcode
+
+#Button click
+def button_clicked():
+    print("Reservering is verzonden")
+
+
