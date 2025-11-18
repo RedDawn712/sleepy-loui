b89c24a4ce21631b15d78d93128bc7996d33424f
Melissa Halley <melissamariahalley@outlook.com>
2025-11-18 20:04:06 +0100

    If statements tryout

--- Biker app.py
+++ Biker app.py
@@ -1,18 +1,37 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
 root = Tk()
 
-root.title("Welkom bij Biker")
+root.title("Fietsreservering bij Biker")
 root.geometry('1024x768')
 
 lbl = Label(root, text= "Bij Biker kom je vooruit.")
 lbl.grid()
 
-lbl1 = Label(root, text= "Persoonsgegevens")
-lbl1.grid(row= 2, column= 0)
-lbl2 = Label(root, text= "Voornaam")
-lbl2.grid(row=3, column=0)
-txt = Entry(root, width= 15)
-txt.grid(row= 3, column=1)
+#Persoonsgegevens invullen
+def persoonsgegevens():
+    lbl1 = Label(root, text= "Persoonsgegevens")
+    lbl1.grid(row= 2, column= 0)
+    lbl2 = Label(root, text= "Voornaam")
+    lbl2.grid(row=3, column=0)
+    txt = Entry(root, width= 15)
+    txt.grid(row= 3, column=1)
+    lbl3 = Label(root, text= "Achternaam")
+    lbl3.grid(row=4, column=0)
+    txt = Entry(root, width= 15)
+    txt.grid(row= 4, column=1)
+    lbl4 = Label(root, text= "Adres")
+    lbl4.grid(row=5, column=0)
+    txt = Entry(root, width= 15)
+    txt.grid(row= 5, column=1)
+    lbl5 = Label(root, text= "Postcode")
+    lbl5.grid(row=6, column=0)
+    txt = Entry(root, width= 15)
+    txt.grid(row= 6, column=1)
 
-mainloop()
+if not persoonsgegevens():
+    messagebox.showerror(title= "Ontbrekende gegevens", message= "Vull alle gegevens in om de reservering af te ronden")
+
+root.mainloop()
