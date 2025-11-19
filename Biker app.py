de0e2543dfa4ed975526719445619da937781cc5
Melissa Halley <melissamariahalley@outlook.com>
2025-11-19 17:09:01 +0100

    Variables entry removed
    added lambda function

--- Biker app.py
+++ Biker app.py
@@ -1,8 +1,8 @@
 from Data import *
+from tkinter import *
+import tkinter as tk
 
 root = Tk()
-
-#Window Title
 root.title("Fietsreservering bij Biker")
 root.geometry('1024x768')
 
@@ -10,42 +10,40 @@ lblslogan = Label(root, text= "Bij Biker kom je vooruit.")
 lblslogan.grid()
 
 
-#Persoonsgegevens invullen
+#Entry velden
 lblpersoonsgegevens = Label(root, text= "Persoonsgegevens").grid(row= 2, column= 0)
 
 lblvoornaam = Label(root, text= "Voornaam").grid(row=3, column=0)
-txt_voornaam = Entry(root, width= 15)
+txt_voornaam = Entry(root, width= 20)
 txt_voornaam.grid(row= 3, column=1)
 
 lblachternaam = Label(root, text= "Achternaam").grid(row=4, column=0)
-txt_achternaam = Entry(root, width= 15)
+txt_achternaam = Entry(root, width= 20)
 txt_achternaam.grid(row= 4, column=1)
 
 lbladres = Label(root, text= "Adres").grid(row=5, column=0)
-txt_adres = Entry(root, width= 15)
+txt_adres = Entry(root, width= 20)
 txt_adres.grid(row= 5, column=1)
 
 lblpostcode = Label(root, text= "Postcode").grid(row=6, column=0)
 vcmd = (root.register(validate_postcode), '%P')
-txt_postcode = tk.Entry(root, width=15, validate='key', validatecommand=vcmd)
+txt_postcode = tk.Entry(root, width=20, validate='key', validatecommand=vcmd)
 txt_postcode.grid(row=6, column=1)
 
 
 lblemail = Label(root, text="E-mail adres").grid(row=7, column=0)
-txt_email = Entry(root, width=15)
+txt_email = Entry(root, width=20)
 txt_email.grid(row=7, column=1)
 
 lbltelefoonnr = Label(root, text="Telefoon nummer").grid(row=8, column=0)
-txt_telefoonnr = Entry(root, width=15)
+txt_telefoonnr = Entry(root, width=20)
 txt_telefoonnr.grid(row=8, column=1)
 
 
 #Button reservering verzenden
 Button = Button(root,
                 text="Reservering versturen",
-                command=button_clicked, bg = 'light green')
+                command= lambda: button_clicked(txt_voornaam, txt_achternaam, txt_adres, txt_postcode, txt_email, txt_telefoonnr))
 Button.grid(row=15, column=15)
-
-persoonsgegevens()
 
 root.mainloop()
--- Data.py
+++ Data.py
@@ -1,16 +1,19 @@
 import re
-import ttkbootstrap
-import tkinter.messagebox
-from tkinter import *
-from tkinter import ttk
 from tkinter import messagebox
 import tkinter as tk
 
 
 
 #Button click function
-def button_clicked():
-    if not txt_voornaam.get().strip() or not txt_achternaam.get().strip() or not txt_adres.get().strip() or not txt_postcode.get().strip() or not txt_email.get().strip() or not txt_telefoonnr.get().strip():
+def button_clicked(txt_voornaam, txt_achternaam, txt_adres,
+                   txt_postcode, txt_email, txt_telefoonnr):
+    if (not txt_voornaam.get().strip()
+            or not txt_achternaam.get().strip()
+            or not txt_adres.get().strip()
+            or not txt_postcode.get().strip()
+            or not txt_email.get().strip()
+            or not txt_telefoonnr.get().strip()):
+
         messagebox.showerror(title="Ontbrekende gegevens", message="Vul alle velden in")
     else:
         messagebox.showinfo(title="Succes!", message="Reservering verstuurd!")
@@ -23,23 +26,5 @@ def validate_postcode(text):
     text = text.upper()
     if not postcode_re.match(text):
         return False
-    try:
-        txt_postcode.delete(0, tk.END)
-        txt_postcode.insert(0, text)
-        txt_postcode.icursor(len(text))
-    except Exception:
-        pass
+
     return True
-
-#Txt names
-def persoonsgegevens():
-    global txt_voornaam, txt_achternaam, txt_adres, txt_postcode, txt_email, txt_telefoonnr
-
-
-#Variabelen Entry-velden
-txt_voornaam = None
-txt_achternaam = None
-txt_adres = None
-txt_postcode = None
-txt_email = None
-txt_telefoonnr = None
